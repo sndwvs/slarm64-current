6be1e39d017a0595dbcd61e332b8c2d12b9ad96e
mara <mara@fail.pp.ua>
2020-06-07 15:22:43 +0300

    tools: build_changelog.sh fixed remove preffix

--- source/tools/build_changelog.sh
+++ source/tools/build_changelog.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 LANG=C
-CHANGELOG="ChangeLog.txt";
-PATH_DISTRO="/mnt/data/shares/linux/slarm64/slarm64-current";
-#PATH_DISTRO="/mnt/data/shares/linux/slackware/packages";
-PREFFIX_DISTRO="slarm64"
+CHANGELOG="ChangeLog.txt"
+PATH_DISTRO=${PATH_DISTRO:-"/mnt/data/shares/linux/slarm64/slarm64-current"}
+PREFFIX_DISTRO=${PREFFIX_DISTRO:-"slarm64"}
 WORK_DIR=$(mktemp -d)
-FILTER=".*\.\(tgz\|txz\)";
-DELIMITER="+--------------------------+";
+FILTER=".*\.\(tgz\|txz\)"
+DELIMITER="+--------------------------+"
 
 
 
@@ -20,25 +19,27 @@ get_last_timestamp() {
 }
 
 mark_new_update() {
-    for pkg in $(cat ${WORK_DIR}/new); do
-        __pkg=$(echo ${pkg} | rev | cut -d '-' -f2- | rev)
-        _pkg=$(echo ${pkg} | rev | cut -d '-' -f4- | rev)
-        if [[ $(grep -e ${__pkg} ${PATH_DISTRO}/${CHANGELOG}) && ! $(grep -e ${pkg} ${PATH_DISTRO}/${CHANGELOG}) ]]; then
-            sed -i "s:$pkg:$pkg\:  Rebuilt\.:" ${WORK_DIR}/new
-        elif [[ $(grep -e ${_pkg} ${PATH_DISTRO}/${CHANGELOG}) && ! $(grep -e ${pkg} ${PATH_DISTRO}/${CHANGELOG}) ]]; then
-            sed -i "s:$pkg:$pkg\:  Upgraded\.:" ${WORK_DIR}/new
-        elif [[ ! $(grep -e ${_pkg} ${PATH_DISTRO}/${CHANGELOG}) ]]; then
-            sed -i "s:$pkg:$pkg\:  Added\.:" ${WORK_DIR}/new
-        else
-            sed -i -e "s:${pkg}::g" -e "/^$/d" ${WORK_DIR}/new
-        fi
-    done
+    if [[ -f ${WORK_DIR}/new ]]; then
+        for pkg in $(cat ${WORK_DIR}/new); do
+            __pkg=$(echo ${pkg} | rev | cut -d '-' -f2- | rev)
+            _pkg=$(echo ${pkg} | rev | cut -d '-' -f4- | rev)
+            if [[ $(grep -e ${__pkg} ${PATH_DISTRO}/${CHANGELOG} 2>/dev/null) && ! $(grep -e ${pkg} ${PATH_DISTRO}/${CHANGELOG} 2>/dev/null) ]]; then
+                sed -i "s:$pkg:$pkg\:  Rebuilt\.:" ${WORK_DIR}/new
+            elif [[ $(grep -e ${_pkg} ${PATH_DISTRO}/${CHANGELOG} 2>/dev/null) && ! $(grep -e ${pkg} ${PATH_DISTRO}/${CHANGELOG} 2>/dev/null) ]]; then
+                sed -i "s:$pkg:$pkg\:  Upgraded\.:" ${WORK_DIR}/new
+            elif [[ ! $(grep -e ${_pkg} ${PATH_DISTRO}/${CHANGELOG} 2>/dev/null) ]]; then
+                sed -i "s:$pkg:$pkg\:  Added\.:" ${WORK_DIR}/new
+            else
+                sed -i -e "s:${pkg}::g" -e "/^$/d" ${WORK_DIR}/new
+            fi
+        done
+    fi
 }
 
 mark_remove() {
-    find $PATH_DISTRO -regex $FILTER -print | rev | cut -d '-' -f4- | rev | sed "s:$PATH_DISTRO\/${PREFFIX_DISTRO}\/::g" > ${WORK_DIR}/current
-    cat "${PATH_DISTRO}/${CHANGELOG}" | grep ${FILTER} | rev | cut -d '-' -f4- | rev | sort | uniq >> ${WORK_DIR}/uniq
-    for f in $(cat "${PATH_DISTRO}/${CHANGELOG}" | grep ${FILTER} | grep Removed | rev | cut -d '-' -f4- | rev); do
+    find $PATH_DISTRO -regex $FILTER -print | rev | cut -d '-' -f4- | rev | sed -e "s:${PATH_DISTRO}\/\|${PREFFIX_DISTRO}\/::g" > ${WORK_DIR}/current
+    cat "${PATH_DISTRO}/${CHANGELOG}" 2>/dev/null | grep ${FILTER} | rev | cut -d '-' -f4- | rev | sort | uniq >> ${WORK_DIR}/uniq
+    for f in $(cat "${PATH_DISTRO}/${CHANGELOG}" 2>/dev/null | grep ${FILTER} | grep Removed | rev | cut -d '-' -f4- | rev); do
         sed -i -e "s:${f}$::g" -e "/^$/d" ${WORK_DIR}/uniq
     done
     for pkg in $(cat ${WORK_DIR}/uniq); do
@@ -50,7 +51,7 @@ mark_remove() {
 FILES=$(find $PATH_DISTRO -newerct "$(get_last_timestamp)" -regex $FILTER)
 
 for pkg in ${FILES}; do
-    echo ${pkg/$PATH_DISTRO\/${PREFFIX_DISTRO}\//} >> ${WORK_DIR}/new
+    echo ${pkg} | sed -e "s:${PATH_DISTRO}\/\|${PREFFIX_DISTRO}\/::g" >> ${WORK_DIR}/new
 done
 
 
@@ -64,7 +65,7 @@ fi
 if [[ -f ${WORK_DIR}/packages ]]; then
     echo "$DELIMITER" >> ${WORK_DIR}/packages
     echo $(date -u) > ${WORK_DIR}/headers
-    echo "$(cat ${WORK_DIR}/headers ${WORK_DIR}/packages ${PATH_DISTRO}/${CHANGELOG})" > ${PATH_DISTRO}/${CHANGELOG}
+    echo "$(cat ${WORK_DIR}/headers ${WORK_DIR}/packages ${PATH_DISTRO}/${CHANGELOG} 2>/dev/null)" > ${PATH_DISTRO}/${CHANGELOG}
 fi
 
 [[ -d $WORK_DIR ]] && rm -rf $WORK_DIR
